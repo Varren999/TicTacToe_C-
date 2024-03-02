7ccf35fca60311b6734ba5ae36014109a3d3fdb2
Varren999 <varren999@gmail.com>
2024-03-02 11:54:23 +0700

    refactoring Game.cs

--- TicTacToe/Game.cs
+++ TicTacToe/Game.cs
@@ -9,136 +9,32 @@ namespace TicTacToe
     internal class Game
     {
         Random random = new Random();
-        int num = 0, step = 0;
-        string msg1 = "Победил игрок играющий крестиком!\n";
-        //string msg1 = "Победил игрок!\n";
-        //string msg2 = "Победил компьютер!\n";
-        string msg2 = "Победил игрок играющий ноликом!\n";
+        int step = 0;
+        string[] msg = new string[] { "Победил игрок играющий крестиком!\n", "Победил игрок играющий ноликом!\n"};
         char cross = 'X', zero = 'O';
         char[] Endaged = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
 
-        string screen = "-------------\n|   |   |   |\n-------------\n|   |   |   |\n-------------\n|   |   |   |\n-------------\n";
         // 1 = 16, 2 = 20, 3 = 24, 4 = 44, 5 = 48, 6 = 52, 7 = 72, 8 = 76, 9 = 80
+        string screen = "-------------\n|   |   |   |\n-------------\n|   |   |   |\n-------------\n|   |   |   |\n-------------\n";
 
         // Проверяем занято поле.
-        bool Is_Endaged(int num, char sim)
+        bool Is_Endaged(int value, char cr)
         {
-            switch (num)
+            if (Endaged[value] != cross && Endaged[value] != zero)
             {
-                case 1:
-                    {
-                        if (Endaged[0] == '1')
-                        {
-                            Endaged[0] = sim;
-                            return true;
-                        }
-                        else
-                            return false;
-                    }
-
-                case 2:
-                    {
-                        if (Endaged[1] == '2')
-                        {
-                            Endaged[1] = sim;
-                            return true;
-                        }
-                        else
-                            return false;
-                    }
-
-                case 3:
-                    {
-                        if (Endaged[2] == '3')
-                        {
-                            Endaged[2] = sim;
-                            return true;
-                        }
-                        else
-                            return false;
-                    }
-
-                case 4:
-                    {
-                        if (Endaged[3] == '4')
-                        {
-                            Endaged[3] = sim;
-                            return true;
-                        }
-                        else
-                            return false;
-                    }
-
-                case 5:
-                    {
-                        if (Endaged[4] == '5')
-                        {
-                            Endaged[4] = sim;
-                            return true;
-                        }
-                        else
-                            return false;
-                    }
-
-                case 6:
-                    {
-                        if (Endaged[5] == '6')
-                        {
-                            Endaged[5] = sim;
-                            return true;
-                        }
-                        else
-                            return false;
-                    }
-
-                case 7:
-                    {
-                        if (Endaged[6] == '7')
-                        {
-                            Endaged[6] = sim;
-                            return true;
-                        }
-                        else
-                            return false;
-                    }
-
-                case 8:
-                    {
-                        if (Endaged[7] == '8')
-                        {
-                            Endaged[7] = sim;
-                            return true;
-                        }
-                        else
-                            return false;
-                    }
-
-                case 9:
-                    {
-                        if (Endaged[8] == '9')
-                        {
-                            Endaged[8] = sim;
-                            return true;
-                        }
-                        else
-                            return false;
-                    }
-
-                default:
-                    {
-                        Console.WriteLine("\x1b[31mВведены некорректные данные!\x1b[0m");
-                        Console.ReadKey(true);
-                        return false;
-                    }
+                Endaged[value] = cr;
+                return true;
             }
+            else
+                return false;
         }
 
         // Ничья
         bool Is_Draw()
         {
-            for (int i = 1; i <= Endaged.Length; i++)
+            for (int i = 0; i < Endaged.Length; i++)
             {
-                if (Is_Endaged(i, Convert.ToChar(i + 48)))
+                if (Is_Endaged(i, Convert.ToChar(i + 49)))
                     return true;
             }
             Console.WriteLine("Ничья\nДля выход нажмите любую кнопку");
@@ -149,158 +45,92 @@ namespace TicTacToe
         // Метод проверяет выигрышные ситуации.
         bool Is_Winner()
         {
-            // cross == 1 == 2 == 3
-            if (cross == Endaged[0] && Endaged[0] == Endaged[1] && Endaged[1] == Endaged[2])
+            // 1 == 2 == 3
+            if (Endaged[0] == Endaged[1] && Endaged[1] == Endaged[2])
             {
-                Console.WriteLine(msg1);
+                Console.WriteLine(msg[step]);
                 Console.ReadKey(true);
                 return false;
             }
 
-            // cross == 4 == 5 == 6
-            if (cross == Endaged[3] && Endaged[3] == Endaged[4] && Endaged[4] == Endaged[5])
+            // 4 == 5 == 6
+            if (Endaged[3] == Endaged[4] && Endaged[4] == Endaged[5])
             {
-                Console.WriteLine(msg1);
+                Console.WriteLine(msg[step]);
                 Console.ReadKey(true);
                 return false;
             }
 
-            // cross == 7 == 8 == 9
-            if (cross == Endaged[6] && Endaged[6] == Endaged[7] && Endaged[7] == Endaged[8])
+            // 7 == 8 == 9
+            if (Endaged[6] == Endaged[7] && Endaged[7] == Endaged[8])
             {
-                Console.WriteLine(msg1);
+                Console.WriteLine(msg[step]);
                 Console.ReadKey(true);
                 return false;
             }
 
-            // cross == 1 == 5 == 9
-            if (cross == Endaged[0] && Endaged[0] == Endaged[4] && Endaged[4] == Endaged[8])
+            // 1 == 5 == 9
+            if (Endaged[0] == Endaged[4] && Endaged[4] == Endaged[8])
             {
-                Console.WriteLine(msg1);
+                Console.WriteLine(msg[step]);
                 Console.ReadKey(true);
                 return false;
             }
 
-            // cross == 3 == 5 == 7
-            if (cross == Endaged[2] && Endaged[2] == Endaged[4] && Endaged[4] == Endaged[6])
+            // 3 == 5 == 7
+            if (Endaged[2] == Endaged[4] && Endaged[4] == Endaged[6])
             {
-                Console.WriteLine(msg1);
+                Console.WriteLine(msg[step]);
                 Console.ReadKey(true);
                 return false;
             }
 
-            // cross == 1 == 4 == 7
-            if (cross == Endaged[0] && Endaged[0] == Endaged[3] && Endaged[3] == Endaged[6])
+            // 1 == 4 == 7
+            if (Endaged[0] == Endaged[3] && Endaged[3] == Endaged[6])
             {
-                Console.WriteLine(msg1);
+                Console.WriteLine(msg[step]);
                 Console.ReadKey(true);
                 return false;
             }
 
-            // cross == 2 == 5 == 8
-            if (cross == Endaged[1] && Endaged[1] == Endaged[4] && Endaged[4] == Endaged[7])
+            // 2 == 5 == 8
+            if (Endaged[1] == Endaged[4] && Endaged[4] == Endaged[7])
             {
-                Console.WriteLine(msg1);
+                Console.WriteLine(msg[step]);
                 Console.ReadKey(true);
                 return false;
             }
 
-            // cross == 3 == 6 == 9
-            if (cross == Endaged[2] && Endaged[2] == Endaged[5] && Endaged[5] == Endaged[8])
+            // 3 == 6 == 9
+            if (Endaged[2] == Endaged[5] && Endaged[5] == Endaged[8])
             {
-                Console.WriteLine(msg1);
+                Console.WriteLine(msg[step]);
                 Console.ReadKey(true);
                 return false;
             }
-
-            // zero == 1 == 2 == 3
-            if (zero == Endaged[0] && Endaged[0] == Endaged[1] && Endaged[1] == Endaged[2])
-            {
-                Console.WriteLine(msg2);
-                Console.ReadKey(true);
-                return false;
-            }
-
-            // zero == 4 == 5 == 6
-            if (zero == Endaged[3] && Endaged[3] == Endaged[4] && Endaged[4] == Endaged[5])
-            {
-                Console.WriteLine(msg2);
-                Console.ReadKey(true);
-                return false;
-            }
-
-            // zero == 7 == 8 == 9
-            if (zero == Endaged[6] && Endaged[6] == Endaged[7] && Endaged[7] == Endaged[8])
-            {
-                Console.WriteLine(msg2);
-                Console.ReadKey(true);
-                return false;
-            }
-
-            // zero == 1 == 5 == 9
-            if (zero == Endaged[0] && Endaged[0] == Endaged[4] && Endaged[4] == Endaged[8])
-            {
-                Console.WriteLine(msg2);
-                Console.ReadKey(true);
-                return false;
-            }
-
-            // zero == 3 == 5 == 7
-            if (zero == Endaged[2] && Endaged[2] == Endaged[4] && Endaged[4] == Endaged[6])
-            {
-                Console.WriteLine(msg2);
-                Console.ReadKey(true);
-                return false;
-            }
-
-            // zero == 1 == 4 == 7
-            if (zero == Endaged[0] && Endaged[0] == Endaged[3] && Endaged[3] == Endaged[6])
-            {
-                Console.WriteLine(msg2);
-                Console.ReadKey(true);
-                return false;
-            }
-
-            // zero == 2 == 5 == 8
-            if (zero == Endaged[1] && Endaged[1] == Endaged[4] && Endaged[4] == Endaged[7])
-            {
-                Console.WriteLine(msg2);
-                Console.ReadKey(true);
-                return false;
-            }
-
-            // zero == 3 == 6 == 9
-            if (zero == Endaged[2] && Endaged[2] == Endaged[5] && Endaged[5] == Endaged[8])
-            {
-                Console.WriteLine(msg2);
-                Console.ReadKey(true);
-                return false;
-            }
-
             return true;
         }
 
         // Ход компьютера
-        void ComLogic(char sim)
+        void ComLogic(char cr)
         {
             bool cycle = true;
             Console.WriteLine("Ход компьютера");
-            Console.WriteLine($"Вы играете {sim}\n");
+            Console.WriteLine($"Вы играете {cr}\n");
             do
             {
-                num = random.Next(8 + 1);
-                if (Is_Endaged(num, sim))
+                if (Is_Endaged(random.Next(9), cr))
                     cycle = false;
             } while (cycle);
             System.Threading.Thread.Sleep(2000);
         }
 
         // Ход игрока
-        void Player(char sim)
+        void Player(char cr)
         {
             bool cycle = true;
             Console.WriteLine("Ход игрока");
-            Console.WriteLine($"Вы играете {sim}\n");
+            Console.WriteLine($"Вы играете {cr}\n");
             try
             {
                 while (cycle)
@@ -312,8 +142,7 @@ namespace TicTacToe
                     }
                     Console.SetCursorPosition(0, 12);
                     Console.Write("Введите номер свободной ячейки: ");
-                    num = Convert.ToInt32(Console.ReadLine());
-                    if (!Is_Endaged(num, sim))
+                    if (!Is_Endaged(Convert.ToInt32(Console.ReadLine()) - 1, cr))
                     {
                         Console.WriteLine("Такой ход уже был!\n");
                         Console.ReadKey(true);
@@ -332,17 +161,17 @@ namespace TicTacToe
         }
 
         // Кто ходит?
-        void Step()
+        private void Step()
         {
             if (step == 0)
             {
-                Player(cross);
-                step++;
+                ComLogic(zero);
+                //Player(zero);
+                step = 1;              
             }
             else
             {
-                //ComLogic(zero);
-                Player(zero);
+                Player(cross);
                 step = 0;
             }
         }
@@ -374,13 +203,14 @@ namespace TicTacToe
         //
         public void Play()
         {
-            step = random.Next(2 + 0);
+            step = random.Next(2);
             do
             {
                 Screen();
                 Step();
                 Screen();
-            } while (Is_Winner() && Is_Draw());
+            }
+            while (Is_Winner() && Is_Draw());
         }
     }
 }
